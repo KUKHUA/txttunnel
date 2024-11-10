35a589cd7643a42ed5a462fb463450d1d9abed4f
A Duck <youarea@duck.com>
2024-11-10 16:39:30 -0500

    update to v3-beta

--- main.go
+++ main.go
@@ -24,14 +24,21 @@ var clientsMutex = &sync.Mutex{}
 func main() {
 	log.Println("Starting server on port 2427")
 	http.HandleFunc("/", withCORS(homePage))
-	http.HandleFunc("/api/v2/tunnel/create", withCORS(createTunnel))
-	http.HandleFunc("/api/v2/tunnel/stream", withCORS(streamTunnelContent))
-	http.HandleFunc("/api/v2/tunnel/send", withCORS(sendToTunnel))
+	http.HandleFunc("/LICENSE", withCORS(giveLicense))
+	http.HandleFunc("/api/v3/tunnel/create", withCORS(createTunnel))
+	http.HandleFunc("/api/v3/tunnel/stream", withCORS(streamTunnelContent))
+	http.HandleFunc("/api/v3/tunnel/get", withCORS(getTunnelContent))
+	http.HandleFunc("/api/v3/tunnel/send", withCORS(sendToTunnel))
 	log.Fatal(http.ListenAndServe(":2427", nil))
 }
 
+func giveLicense(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "web/LICENSE.txt")
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the TXTTunnel homepage!")
+	// Return the home page /web/index.html
+	http.ServeFile(w, r, "web/index.html")
 }
 
 func withCORS(handler http.HandlerFunc) http.HandlerFunc {
@@ -47,69 +54,129 @@ func withCORS(handler http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
-func sendToTunnel(w http.ResponseWriter, r *http.Request) {
-	var requestData struct {
-		ID         string `json:"id"`
-		Content    string `json:"content"`
-		SubChannel string `json:"subChannel"`
+func getTunnelContent(w http.ResponseWriter, r *http.Request) {
+	tunnelId := ""
+	subChannel := ""
+	if r.Method == http.MethodGet {
+		tunnelId = r.URL.Query().Get("id")
+		subChannel = r.URL.Query().Get("subChannel")
+		if r.URL.Query().Get("subchannel") != "" {
+			subChannel = r.URL.Query().Get("subchannel")
+		}
+		if r.URL.Query().Get("ID") != "" {
+			tunnelId = r.URL.Query().Get("ID")
+		}
+	} else if r.Method == http.MethodPost {
+		requestBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read the request body", http.StatusInternalServerError)
+			return
+		}
+
+		// get 'id' from the request body JSON
+		var requestBodyJSON map[string]string
+		err = json.Unmarshal(requestBody, &requestBodyJSON)
+		if err != nil {
+			http.Error(w, "Failed to parse the request body", http.StatusInternalServerError)
+			return
+		}
+
+		if requestBodyJSON["subchannel"] != "" {
+			subChannel = requestBodyJSON["subchannel"]
+		}
+
+		if requestBodyJSON["subChannel"] != "" {
+			subChannel = requestBodyJSON["subChannel"]
+		}
+
+		if requestBodyJSON["ID"] != "" {
+			tunnelId = requestBodyJSON["ID"]
+		}
+
+		if requestBodyJSON["id"] != "" {
+			tunnelId = requestBodyJSON["id"]
+		}
 	}
 
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-		return
+	if subChannel == "" {
+		subChannel = "main"
 	}
 
-	err = json.Unmarshal(body, &requestData)
-	if err != nil {
-		http.Error(w, "Failed to parse JSON", http.StatusBadRequest)
-		return
-	}
-
-	if requestData.ID == "" {
-		http.Error(w, "No tunnel id has been provided.", http.StatusBadRequest)
-		return
-	}
-
-	if requestData.Content == "" {
-		http.Error(w, "No content has been provided.", http.StatusBadRequest)
-		return
-	}
-
-	if requestData.SubChannel == "" {
-		http.Error(w, "No subChannel has been provided.", http.StatusBadRequest)
+	if tunnelId == "" {
+		http.Error(w, "The request must contain a valid 'id' parameter or field", http.StatusBadRequest)
 		return
 	}
 
 	tunnelsMutex.Lock()
-	tunnel, exists := tunnels[requestData.ID]
+	tunnel, exists := tunnels[tunnelId]
 	if !exists {
 		tunnelsMutex.Unlock()
-		http.Error(w, "No tunnel with this id exists.", http.StatusInternalServerError)
+		http.Error(w, "No tunnel with this id exists.", http.StatusNotFound)
 		return
 	}
-	tunnel.SubChannels[requestData.SubChannel] = requestData.Content
-	tunnelsMutex.Unlock()
-
-	clientsMutex.Lock()
-	for _, client := range clients[requestData.ID][requestData.SubChannel] {
-		client <- requestData.Content
+	// Return the content of the subchannel if it exists
+	if tunnel.SubChannels[subChannel] != "" {
+		w.Header().Set("Content-Type", "application/json")
+		response, err := json.Marshal(map[string]string{"content": tunnel.SubChannels[subChannel]})
+		if err != nil {
+			http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+			return
+		}
+		w.Write(response)
 	}
-	clientsMutex.Unlock()
-
-	log.Printf("Tunnel %s subChannel %s has been updated.", requestData.ID, requestData.SubChannel)
-	fmt.Fprintf(w, "Tunnel %s subChannel %s has been updated.", requestData.ID, requestData.SubChannel)
+	tunnelsMutex.Unlock()
 }
 
 func streamTunnelContent(w http.ResponseWriter, r *http.Request) {
-	tunnelId := r.URL.Query().Get("id")
-	subChannel := r.URL.Query().Get("subChannel")
-	if tunnelId == "" {
-		http.Error(w, "No tunnel id has been provided.\nPlease use ?id= to include the tunnel id.", http.StatusBadRequest)
-		return
+	tunnelId := ""
+	subChannel := ""
+	if r.Method == http.MethodGet {
+		tunnelId = r.URL.Query().Get("id")
+		subChannel = r.URL.Query().Get("subChannel")
+		if r.URL.Query().Get("subchannel") != "" {
+			subChannel = r.URL.Query().Get("subchannel")
+		}
+		if r.URL.Query().Get("ID") != "" {
+			tunnelId = r.URL.Query().Get("ID")
+		}
+	} else if r.Method == http.MethodPost {
+		requestBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read the request body", http.StatusInternalServerError)
+			return
+		}
+
+		// get 'id' from the request body JSON
+		var requestBodyJSON map[string]string
+		err = json.Unmarshal(requestBody, &requestBodyJSON)
+		if err != nil {
+			http.Error(w, "Failed to parse the request body", http.StatusInternalServerError)
+			return
+		}
+
+		if requestBodyJSON["subchannel"] != "" {
+			subChannel = requestBodyJSON["subchannel"]
+		}
+
+		if requestBodyJSON["subChannel"] != "" {
+			subChannel = requestBodyJSON["subChannel"]
+		}
+
+		if requestBodyJSON["ID"] != "" {
+			tunnelId = requestBodyJSON["ID"]
+		}
+
+		if requestBodyJSON["id"] != "" {
+			tunnelId = requestBodyJSON["id"]
+		}
 	}
+
 	if subChannel == "" {
-		http.Error(w, "No subChannel has been provided.\nPlease use ?subChannel= to include the subChannel.", http.StatusBadRequest)
+		subChannel = "main"
+	}
+
+	if tunnelId == "" {
+		http.Error(w, "The request must contain a valid 'id' parameter or field", http.StatusBadRequest)
 		return
 	}
 
@@ -117,7 +184,7 @@ func streamTunnelContent(w http.ResponseWriter, r *http.Request) {
 	_, exists := tunnels[tunnelId]
 	if !exists {
 		tunnelsMutex.Unlock()
-		http.Error(w, "No tunnel with this id exists.", http.StatusInternalServerError)
+		http.Error(w, "No tunnel with this id exists.", http.StatusNotFound)
 		return
 	}
 	tunnelsMutex.Unlock()
@@ -153,18 +220,170 @@ func streamTunnelContent(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func createTunnel(w http.ResponseWriter, r *http.Request) {
-	tunnelId := fmt.Sprintf("%02d%c%02d%c%02d%c", rand.Intn(100), 'A'+rune(rand.Intn(26)), rand.Intn(100), 'A'+rune(rand.Intn(26)), rand.Intn(100), 'A'+rune(rand.Intn(26)))
-	tunnelsMutex.Lock()
-	tunnels[tunnelId] = &Tunnel{ID: tunnelId, Content: "", SubChannels: make(map[string]string)}
-	tunnelsMutex.Unlock()
+func sendToTunnel(w http.ResponseWriter, r *http.Request) {
+	// we need the id, subchannel and content
+	if r.Method == http.MethodPost {
+		requestBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read the request body", http.StatusInternalServerError)
+			return
+		}
 
-	w.Header().Set("Content-Type", "application/json")
-	response, err := json.Marshal(map[string]string{"id": tunnelId})
-	if err != nil {
-		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		// get 'id', 'subchannel' and 'content' from the request body JSON
+		var requestBodyJSON map[string]string
+		err = json.Unmarshal(requestBody, &requestBodyJSON)
+		if err != nil {
+			http.Error(w, "Failed to parse the request body", http.StatusInternalServerError)
+			return
+		}
+
+		if requestBodyJSON["subchannel"] != "" {
+			requestBodyJSON["subChannel"] = requestBodyJSON["subchannel"]
+		}
+
+		if requestBodyJSON["subChannel"] == "" {
+			requestBodyJSON["subChannel"] = "main"
+		}
+
+		if requestBodyJSON["ID"] != "" {
+			requestBodyJSON["id"] = requestBodyJSON["ID"]
+		}
+
+		if requestBodyJSON["id"] == "" || requestBodyJSON["subChannel"] == "" || requestBodyJSON["content"] == "" {
+			http.Error(w, "The request body must contain a valid 'id', 'subChannel' and 'content' field", http.StatusBadRequest)
+			return
+		}
+
+		tunnelsMutex.Lock()
+		tunnel, exists := tunnels[requestBodyJSON["id"]]
+		if !exists {
+			tunnelsMutex.Unlock()
+			http.Error(w, "No tunnel with this id exists.", http.StatusNotFound)
+			return
+		}
+		tunnel.SubChannels[requestBodyJSON["subChannel"]] = requestBodyJSON["content"]
+		tunnelsMutex.Unlock()
+
+		clientsMutex.Lock()
+		for _, client := range clients[requestBodyJSON["id"]][requestBodyJSON["subChannel"]] {
+			client <- requestBodyJSON["content"]
+		}
+		clientsMutex.Unlock()
+
+		w.WriteHeader(http.StatusOK)
+	} else if r.Method == http.MethodGet {
+		id := r.URL.Query().Get("id")
+		subChannel := r.URL.Query().Get("subChannel")
+		content := r.URL.Query().Get("content")
+
+		if r.URL.Query().Get("subchannel") != "" {
+			subChannel = r.URL.Query().Get("subchannel")
+		}
+
+		if r.URL.Query().Get("ID") != "" {
+			id = r.URL.Query().Get("ID")
+		}
+
+		if id == "" || subChannel == "" || content == "" {
+			http.Error(w, "The request must contain a valid 'id', 'subChannel' and 'content' parmerters", http.StatusBadRequest)
+			return
+		}
+
+		tunnelsMutex.Lock()
+		tunnel, exists := tunnels[id]
+		if !exists {
+			tunnelsMutex.Unlock()
+			http.Error(w, "No tunnel with this id exists.", http.StatusNotFound)
+			return
+		}
+		tunnel.SubChannels[subChannel] = content
+		tunnelsMutex.Unlock()
+
+		clientsMutex.Lock()
+		for _, client := range clients[id][subChannel] {
+			client <- content
+		}
+		clientsMutex.Unlock()
+		w.WriteHeader(http.StatusOK)
+	} else {
+		http.Error(w, "Method not allowed. Only POST requests are allowed.", http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write(response)
-	log.Printf("Tunnel %s has been created.", tunnelId)
+}
+
+func createTunnel(w http.ResponseWriter, r *http.Request) {
+	// check if it's a POST or GET request
+	if r.Method == http.MethodPost {
+		requestBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read the request body", http.StatusInternalServerError)
+			return
+		}
+
+		// get 'id' from the request body JSON
+		var requestBodyJSON map[string]string
+		err = json.Unmarshal(requestBody, &requestBodyJSON)
+		if err != nil {
+			http.Error(w, "Failed to parse the request body", http.StatusInternalServerError)
+			return
+		}
+
+		if requestBodyJSON["id"] == "" {
+			http.Error(w, "The request body must contain a vaild 'id' field", http.StatusBadRequest)
+		}
+
+		tunnelsMutex.Lock()
+		tunnels[requestBodyJSON["id"]] = &Tunnel{ID: requestBodyJSON["id"], Content: "", SubChannels: make(map[string]string)}
+		tunnelsMutex.Unlock()
+
+		response, err := json.Marshal(map[string]string{"id": requestBodyJSON["id"]})
+
+		if err != nil {
+			http.Error(w, "Error creating the tunnel", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(response)
+	} else if r.Method == http.MethodGet {
+		if r.URL.Query().Get("id") == "" {
+			// The user used the GET method but didn't provide an ID
+			tunnelId := generateRandomID(4)
+			tunnelsMutex.Lock()
+			tunnels[tunnelId] = &Tunnel{ID: tunnelId, Content: "", SubChannels: make(map[string]string)}
+			tunnelsMutex.Unlock()
+
+			w.Header().Set("Content-Type", "application/json")
+			response, err := json.Marshal(map[string]string{"id": tunnelId})
+			if err != nil {
+				http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+				return
+			}
+			w.Write(response)
+		} else {
+			// The user used the GET method and provided an ID
+			tunnelsMutex.Lock()
+			tunnels[r.URL.Query().Get("id")] = &Tunnel{ID: r.URL.Query().Get("id"), Content: "", SubChannels: make(map[string]string)}
+			tunnelsMutex.Unlock()
+			response, err := json.Marshal(map[string]string{"id": r.URL.Query().Get("id")})
+			if err != nil {
+				http.Error(w, "Error creating the tunnel", http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(response)
+		}
+	} else {
+		http.Error(w, "Method not allowed. Only POST and GET requests are allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func generateRandomID(amount int) string {
+	const charset = "ABCDEFGHJKLMNPQRSTUVWXYZ123456789`~!@#$%^&*()_-+=[]{}|;:',.<>/?"
+	b := make([]byte, amount)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
 }
